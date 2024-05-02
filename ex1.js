ba1205a085875be81f9d77328cd385d44d96c56c
Minzyx <minzybtw@gmail.com>
2024-05-02 17:26:37 -0300

    feat: Exercicio 1 feito

---
+++ ex1.js
@@ -0,0 +1,29 @@
+// == -> Verifica se um valor é igual à outro
+// === -> Verifica se o valor e o tipo dos itens são iguais
+
+
+let username = prompt("Digite um nome de usuário:")
+let senha = prompt("Digite a senha:")
+
+// if (username == "admin") {
+//     console.log ("Você é admin");
+
+//     if (senha == "senha123"){
+//         console.log ("Bem vindo ADMIN!")
+//     }
+// }
+
+if (username == "admin" && senha == "senha 123") {
+    console.log("Login bem sucedido!")
+} else{
+    console.log ("Você não tem permissão de acesso!")
+}
+
+
+
+
+
+
+
+
+
