dcfba9fa8b5e4026c91a126ca24ad5dc497c34c7
Minzyx <minzybtw@gmail.com>
2024-05-03 15:20:05 -0300

    feat: exercicio 3 par ou impar

---
+++ ex3.js
@@ -0,0 +1,15 @@
+// Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+
+let numero = parseInt(prompt("Digite um número:"));
+
+
+if(numero % 2 === 0) {
+    console.log(numero + " O número é par.");
+} 
+
+else if (numero % 2 === 1){
+    console.log(numero + " O número ímpar.");
+} else{
+    console.log ("O valor informado não é um número.")
+}
