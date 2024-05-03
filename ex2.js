dc75b7345b6f405c0721ee6bfee1895de69d857a
Minzyx <minzybtw@gmail.com>
2024-05-03 15:17:43 -0300

    feat: exercicio 2 programa do dia da semana.

---
+++ ex2.js
@@ -0,0 +1,29 @@
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana e exibe o nome correspondente a esse dia
+// . Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante.
+
+let dia = prompt("Digite um número de 1 a 7")
+
+switch (dia) {
+
+    case "1":
+        alert("Este número representa segunda-feira!")
+        break
+    case "2":
+        alert("Este número representa terça-feira!")
+        break
+    case "3":
+        alert("Este número representa quarta-feira!")
+        break
+    case "4":
+        alert("Este número representa quinta-feira!")
+        break
+    case "5":
+        alert("Este número representa sexta-feira!")
+        break
+    case "6":
+        alert("Este número representa sábado!")
+        break
+    case "7":
+        alert("Este número representa domingo!")
+        
+    }
