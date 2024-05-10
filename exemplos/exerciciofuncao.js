57622ec0c74415f3fdcfb5b5736640d9c76b8f34
Minzyx <minzybtw@gmail.com>
2024-05-10 14:46:54 -0300

    feat: Exercício função.

--- exemplos/exerciciofuncao.js
+++ exemplos/exerciciofuncao.js
@@ -5,19 +5,20 @@
 
 // O usuário deverá digitar 0 para sair do menu.
 
-let opcao = prompt("Escolha uma opção: \n1 - Exemplo Hello World \n2 - Exemplo Repeticao\n3 - Tabuada")
+let opcao = prompt("Escolha uma opção: \n1 - Soma de todos os números de 1 a 100 \n2 - Contador de 1 a 10\n3 - Tabuada \n4- Contagem Regressiva \n5- Maior ou menor de idade \n6- Par ou Impar")
 
 
-function exemploRepeticao(){
-    let opcaoMenu = prompt("EXERCICIO DE REPETICAO\nDigite uma opção\nDigite 0 para sair")
+function soma(){
+    let a = 1
+    let b = 0
     
-    while (opcaoMenu != "0"){
-        console.log("Repetiiiuu")
+    while (a <= 100) {
+        b += a++
         
-        opcaoMenu = prompt ("R: Digite em uma opção\nDigite 0 para sair: ")
+    
     }
     
-    console.log("Saaiuuuu")
+    console.log("A soma de todos os números de 1 a 100 é: " + b);
 }
 
 function tabuada(){
@@ -31,23 +32,77 @@ if (!isNaN(num)){
     }
 
 }
-    
-    
+
+}
+
+function contador() {
+    for (let cont= 10; cont >=1; cont--) {
+        console.log(cont);
+        
+    }
+}
+
+function contagem() {
+    let num = parseInt(prompt("Insira um número"));
+
+while (num >= 0) {
+   console.log(num);
+
+    num--;
+
+
+}
+
+console.log("Contagem regressiva finalizada!")
+}
+
+function idade() {
+    let idade = 0
+while (idade < 18) {
+    idade = parseInt(prompt("Insira sua idade:"))
+    if (idade < 18) {
+       console.log("Você é menor de idade, digte novamente.");
+    }
+}
+
+console.log("Você é maior de idade!")
+}
+
+function parouimpar() {
+    let num = parseInt(prompt("Digite um número inteiro para saber se ele é impar ou par!"))
+    if (num%2 == 0 ){
+        console.log("Esse número é par!")
+    }else{
+        console.log("Esse número é ímpar!")
+    }
+
 }
 
 
 
 switch (opcao) {
     case "1":
-        console.log("Hello World");
+       soma()
         break;
 
     case "2":
-      exemploRepeticao()
+     contador()
       break;
 
     case "3":
       tabuada()
+      break;
+
+    case "4":
+      contagem()
+      break;
+
+    case "5":
+      idade()
+      break;
+
+    case "6":
+      parouimpar()
       break;
 
 
